20442ac92fc3688e354569c0a91ab93b26a39369
Leevan001 <1249824014@qq.com>
2022-11-14 00:28:28 +0800

    commit

---
+++ JavaScript.js
@@ -0,0 +1,23 @@
+﻿const deg = 6
+const hr = document.querySelector('#hr')
+const mn = document.querySelector('#mn')
+const sc = document.querySelector('#sc')
+
+setInterval(() => {
+    let day = new Date()
+    let hh = day.getHours() * 30
+    let mm = day.getMinutes() * deg
+    let ss = day.getSeconds() * deg
+
+    hr.style.transform = `rotateZ(${hh + (mm / 12)}deg)`
+    mn.style.transform = `rotateZ(${mm}deg)`
+    sc.style.transform = `rotateZ(${ss}deg)`
+
+    var hour = day.getHours();
+    var minute = day.getMinutes();
+    var second = day.getSeconds();
+    if (hour < 10) hour = "0" + hour;
+    if (minute < 10) minute = "0" + minute;
+    if (second < 10) second = "0" + second;
+    document.getElementById("Time").innerText =hour + ":" + minute + ":" + second;
+}, 1000)
